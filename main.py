6d4a35c84bbc343e62ec006acc2d34b2cb4071e7
shruti-bhopale23 <sbhopale34@gmail.com>
2026-06-10 17:51:03 +0530

    Basic python chatbot

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 def chatbot():
     print("Bot:Hello! Type 'bye' to exit")
     while True:
-        user_input = input ("")
-        
+        user_input = input ("You: ").lower()
+
+        if user_input in ["hello","hi","hey"]:
+            print("Bot: Hi there!")
+
+        elif "how are you" in user_input:
+            print("BOt: i am doing great!")    
+        elif "name" in user_input:
+            print("Bot:I am your Python chatbot")
+        elif user_input == "bye":
+            print("bot: Goodbye!")
+            break
+        else:
+            print("Bot: I am confused")
+
+
+chatbot()
